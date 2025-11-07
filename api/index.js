e9021425b50740e336f0e2fbf329713355c79ab9
Hiran Tiago Lins Borba <107688645+htiagolborba@users.noreply.github.com>
2025-11-07 18:44:18 +0000

    Refactor comments and improve error messages

--- api/index.js
+++ api/index.js
@@ -4,73 +4,98 @@ const projectData = require('../data/projectData');
 
 const app = express();
 
-// Set EJS as the view engine
+        //set ejs as the view engine:
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../views'));
 
-// Serve static files from the public folder
+    //serve static files from the /public folder
 app.use(express.static(path.join(__dirname, '../public')));
 
-// Initialize data once
-let initialized = false;
 
-const initializeData = async () => {
-  if (!initialized) {
+    //start data once
+let initialized=false;
+
+const initializeData = async ()=>{
+  if (!initialized){
     await projectData.initialize();
     initialized = true;
+  
   }
+
+
 };
 
-// Home route
-app.get('/', async (req, res) => {
-  try {
+        //--> home route
+app.get('/', async (req, res)=>{
+  
+  try{
     await initializeData();
     const projects = await projectData.getAllProjects();
     res.render('home', { projects: projects.slice(0, 3) });
-  } catch (err) {
+  } 
+  catch (err){
     res.status(404).render('404', {
-      message: "I'm sorry, we're unable to find what you're looking for"
+      message: "Im sorry, we are unable to find what you're looking for"
     });
+  
   }
 });
 
-// About route
-app.get('/about', (req, res) => {
+//about me route
+app.get('/about', (req, res)=>{
   res.render('about');
 });
 
-// Projects route with optional sector query parameter
-app.get('/solutions/projects', async (req, res) => {
-  try {
+//projects route...
+app.get('/solutions/projects', async (req, res)=>{
+  try{
     await initializeData();
-    if (req.query.sector) {
+    
+    if (req.query.sector){
       const projects = await projectData.getProjectsBySector(req.query.sector);
       res.render('projects', { projects: projects });
-    } else {
+    } 
+    
+    else{
       const projects = await projectData.getAllProjects();
       res.render('projects', { projects: projects });
+    
     }
-  } catch (err) {
+  } 
+  catch (err){
     res.status(404).render('404', { message: err });
   }
+
+
 });
 
-// Individual project route
-app.get('/solutions/projects/:id', async (req, res) => {
-  try {
+    //individual project route:
+app.get('/solutions/projects/:id', async (req, res)=>{
+  
+    try{
+
     await initializeData();
     const project = await projectData.getProjectById(req.params.id);
     res.render('project', { project: project });
-  } catch (err) {
+       } 
+  
+    catch (err){
     res.status(404).render('404', { message: err });
-  }
+                }
 });
 
-// 404 route - must be last
-app.use((req, res) => {
-  res.status(404).render('404', {
-    message: "I'm sorry, we're unable to find what you're looking for"
-  });
+//404 route --> must be the last
+app.use((req, res)=>{
+  res.status(404).render('404', {message: "Im sorry, we are unable to find what you're looking for"});
+
 });
 
 module.exports = app;
+
+
+
+
+
+
+
+
